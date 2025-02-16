65f69fe42bca60fe6b78b8b6728ea786f288eb16
Cat Miller <catmiller.cm@gmail.com>
2025-02-15 20:56:53 -0600

    adjust share button css import

--- src/components/atoms/share-button/index.js
+++ src/components/atoms/share-button/index.js
@@ -1,5 +1,5 @@
 // import html2canvas from 'html2canvas';
-import styles from './button.module.css';
+import styles from './share-button.module.css';
 
 const ShareButton = () => {
       // const shareResult = async () => {
