bd5e6e9b7be74ef5fa0983df3c837826540bb14e
Cat Miller <catmiller.cm@gmail.com>
2025-02-18 20:58:35 -0600

    adjust order of replacements

--- src/components/atoms/share-button/index.js
+++ src/components/atoms/share-button/index.js
@@ -8,7 +8,7 @@ const ShareButton = ({ resultRef }) => {
 
     const elementsToBeModified = idsToBeModified.map((id) => document.getElementById(id));
     const originalTexts = elementsToBeModified.map((el) => el.textContent);
-    const modifiedTexts = elementsToBeModified.map((el) => el.textContent.replace(/you are/ig, "I am").replace(/you/ig, "I").replace("Your", "My").replace("your", "my"));
+    const modifiedTexts = elementsToBeModified.map((el) => el.textContent.replace(/you are/ig, "I am").replace("Your", "My").replace("your", "my").replace(/you/ig, "I"));
     elementsToBeModified.forEach((el, index) => el.textContent = modifiedTexts[index]);
 
     try {
