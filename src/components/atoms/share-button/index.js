96c35a2b65dbd0700a8ee45fb1aacaa6ad11dc7a
Cat Miller <catmiller.cm@gmail.com>
2025-02-20 17:37:17 -0500

    add share image loader and add workaround to ensure images are loaded before share

--- src/components/atoms/share-button/index.js
+++ src/components/atoms/share-button/index.js
@@ -1,9 +1,16 @@
+'use client';
+
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import { toPng } from 'html-to-image';
 import styles from './share-button.module.css';
 
 const ShareButton = ({ resultRef, correctAnswers }) => {
+  const [stateOfShare, setStateOfShare] = useState('Share My Result');
+
   const shareResult = async () => {
+    setStateOfShare('Preparing...');
+
     const idsToBeModified = ['score', 'animal', 'description'];
 
     const elementsToBeModified = idsToBeModified.map((id) => document.getElementById(id));
@@ -12,11 +19,18 @@ const ShareButton = ({ resultRef, correctAnswers }) => {
     elementsToBeModified.forEach((el, index) => el.textContent = modifiedTexts[index]);
 
     try {
+      await toPng(resultRef.current);
+      await toPng(resultRef.current);
+      await toPng(resultRef.current);
+
       const dataUrl = await toPng(resultRef.current);
       const blob = await fetch(dataUrl).then((res) => res.blob());
   
       if (blob && navigator.canShare) {
         const file = new File([blob], 'Costa Eu-rica Result.png', { type: 'image/png' });
+
+        setStateOfShare('Ready');
+
         await navigator.share({
           title: "I just finished the Costa Eu-rica Quiz! 🎉",
           text: `I scored ${correctAnswers}/10! Try it yourself at https://costa-rica.cat-miller.com"`,
@@ -24,12 +38,15 @@ const ShareButton = ({ resultRef, correctAnswers }) => {
         });
       } else {
         alert('Your browser does not support sharing images.');
+        setStateOfShare('Could Not Share');
       }
     } catch(error) {
       console.error('Error capturing the result:', error);
+      setStateOfShare('Could Not Share');
     } finally {
       elementsToBeModified.forEach((el, index) => el.textContent = originalTexts[index]);
     }
+    setStateOfShare('Share My Result');
   };
 
   return (
@@ -38,7 +55,7 @@ const ShareButton = ({ resultRef, correctAnswers }) => {
       className={styles.shareButton}
       onClick={shareResult}
     >
-      Share My Result
+      {stateOfShare}
     </button>
 
   )
