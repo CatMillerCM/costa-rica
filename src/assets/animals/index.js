aea427ef4b079781b0e0f939dd920571c202b5c4
Cat Miller <catmiller.cm@gmail.com>
2025-02-19 20:31:25 -0500

    bring in all animal images as assets

--- src/assets/animals/index.js
+++ src/assets/animals/index.js
@@ -1,13 +1,29 @@
+import basiliskLizard from './basilisk-lizard.png';
 import capuchinMonkey from './capuchin-monkey.png';
+import hangingSloth from './hanging-sloth.png';
+import howlerMonkey from './howler-monkey.png';
+import hummingbird from './hummingbird.png';
+import iguana from './iguana.png';
 import jaguar from './jaguar.png';
+import redEyedFrog from './red-eyed-frog.png';
+import scarletMacaw from './scarlet-macaw.png';
 import sloth from './sloth.png';
 import toucan from './toucan.png';
+import turtle from './turtle.png';
 
 const animalImages = {
+  "basilisk lizard": basiliskLizard,
   "capuchin monkey": capuchinMonkey,
+  "hanging sloth": hangingSloth,
+  "howler monkey": howlerMonkey,
+  hummingbird,
+  iguana,
   jaguar,
+  "red-eyed frog": redEyedFrog,
+  "scarlet macaw": scarletMacaw,
   sloth,
-  toucan
+  toucan,
+  turtle
 }
 
 export { animalImages };
