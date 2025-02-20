70c7bd2bdb3489c60d50786ae41fb3a542fadda5
Cat Miller <catmiller.cm@gmail.com>
2025-02-19 20:43:37 -0500

    add animals to logo

--- src/components/atoms/logo/index.js
+++ src/components/atoms/logo/index.js
@@ -1,10 +1,24 @@
+
+import Image from 'next/image';
+import hangingSloth from '@/assets/animals/hanging-sloth.png';
+import scarletMacaw from '@/assets/animals/scarlet-macaw.png';
 import styles from './logo.module.css';
 
 const Logo = ({  }) => {
   return (
-    <div>
-      <p className={styles.costa}>COSTA</p>
-      <p className={styles.rica}>EU&#8209;RICA!</p>
+    <div className={styles.logo}>
+      <Image
+        className={styles.scarletMacaw}
+        src={scarletMacaw}
+        alt='scarlet macaw'
+      />
+      <h1 className={styles.costa}>COSTA</h1>
+      <h1 className={styles.rica}>EU&#8209;RICA!</h1>
+      <Image
+        className={styles.hangingSloth}
+        src={hangingSloth}
+        alt='sloth'
+      />
     </div>
   )
 };
