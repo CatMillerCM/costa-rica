e0000360f7afde8fcbcea962cc19842eb7cb3888
Cat Miller <catmiller.cm@gmail.com>
2025-02-21 10:57:54 -0500

    css adjustments

--- src/components/atoms/answer/index.js
+++ src/components/atoms/answer/index.js
@@ -5,10 +5,8 @@ const Answer = ({ result, answer }) => {
   return (
     <div className={styles.answer}>
       <p>That is {result}!</p>
-      <div className={styles.correctAnswer}>
-        <p>{result === 'correct' ? 'Well done!' : 'The correct answer is:'}</p>
-        {result === 'incorrect' && <p>{answer}</p>}
-      </div>
+      <p>{result === 'correct' ? 'Well done!' : 'The correct answer is:'}</p>
+      {result === 'incorrect' && <p>{answer}</p>}
     </div>
   )
 };
